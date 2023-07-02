0d95ff4a81d5c28c03a0d4a1ea752c0c944394be
Deniz <denistuncbilek92718@gmail.com>
2023-07-02 17:06:19 +0300

    Домашние задание 1

--- app/src/main/java/com/example/playlistmaker2/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker2/SettingsActivity.kt
@@ -12,10 +12,18 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val buttonShare = findViewById<ImageButton>(R.id.s)
         val buttonBack = findViewById<ImageButton>(R.id.back)
         buttonBack.setOnClickListener {
             finish()
         }
+        buttonShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("plain/text")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/")
+            startActivity(Intent.createChooser(shareIntent,"Поделиться"))
+        }
+
 
     }
 }
