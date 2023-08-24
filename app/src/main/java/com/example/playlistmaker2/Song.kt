30c0605f958bc8e307df952d52e30eb78e52410f
Denis <denistuncbilek92718@gmail.com>
2023-08-24 20:59:15 +0300

    Домашние задание 13

--- app/src/main/java/com/example/playlistmaker2/Song.kt
+++ app/src/main/java/com/example/playlistmaker2/Song.kt
@@ -5,7 +5,17 @@ import com.google.gson.annotations.SerializedName
 data class Song(
     val trackName: String,
     val artistName: String,
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String,
     @SerializedName("trackTimeMillis")val trackTime: String,
     val artworkUrl100: String
 ) {
+
+        fun getCoverArtwork() = artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")
+
+        fun getYear() = releaseDate.substring(0,4)
+
+
 }
